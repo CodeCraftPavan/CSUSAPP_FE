ad5d09e3a5053503b0ebc8ebca5ba28fceca9c6d
Pavan Kumar <pavan.kumar@ecanarys.com>
2024-08-27 11:00:39 +0530

    feat:added login router

--- CSUSAPP/src/app/app-routing.module.ts
+++ CSUSAPP/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { CustomersListComponent } from './customer/customers-list/customers-list.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'login',pathMatch: 'full'},
   {path: 'login', component:LoginComponent}, 
+  {path: 'customer/customersList', component:CustomersListComponent}, 
   // {path: '**',redirectTo: 'login'},
   {
     path: 'customer',loadChildren: () => import('./customer/customer.module').then((m) => m.CustomerModule),
